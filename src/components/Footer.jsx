ebe2192d8c2c2da70d279c1ea3f9c7649261ee6e
Milad <Miladsafari386@gmail.com>
2025-04-17 17:51:50 +0330

    Edit-footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,9 +16,8 @@ function Footer() {
                     <div className="left">
                         <div className="title">ما را در شبکه های اجتماعی دنبال کنید</div>
                         <div className="socials">
-                            <a href="mailto:miladsafarishop@gmail.com" aria-label="انتقال به صفحه توییتر" className="footer-item"><i className="fab fa-twitter"></i></a>
-                            <a href="mailto:miladsafarishop@gmail.com" aria-label="انتقال به صفحه لینکدین" className="footer-item"><i className="fa-brands fa-instagram"></i></a>
-                            <a href="mailto:miladsafarishop@gmail.com" aria-label="انتقال به صفحه تلگرام" className="footer-item"><i className="fab fa-telegram"></i></a>
+                            <a href="mailto:miladsafarishop@gmail.com" aria-label="انتقال به صفحه گیت هاب" className="footer-item"><i className="fa-brands fa-github"></i></a>
+                            <a href="mailto:miladsafarishop@gmail.com" aria-label="انتقال به صفحه لینکدین" className="footer-item"><i className="fa-brands fa-linkedin-in"></i></a>
                             <a href="mailto:miladsafarishop@gmail.com" aria-label="ارسال ایمیل به سازنده سایت" className="footer-item"><i className="fa-solid fa-at"></i></a>
                         </div>
                     </div>
