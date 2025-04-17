b359ca61ab65e86c0429b9bedba11a5706418ef0
Milad <Miladsafari386@gmail.com>
2025-04-17 18:06:14 +0330

    V1.1-Kharazmi

--- src/components/ProfileMenu.jsx
+++ src/components/ProfileMenu.jsx
@@ -93,12 +93,12 @@ const ProfileMenu = () => {
                 </svg>
                 <span style={{ cursor: "pointer" }}>سفارشات من</span>
               </Link>
-              <hr />
-              {(userData.role === "Admin" || userData.role === "Owner") && <Link to="/AdminDashboard" aria-label="پنل ادمین" className="ProfileMenu-btn">
+              
+              {(userData.role === "Admin" || userData.role === "Owner") && <><hr /><Link to="/AdminDashboard" aria-label="پنل ادمین" className="ProfileMenu-btn">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-fill-lock" viewBox="0 0 16 16">
                   <path d="M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0m-9 8c0 1 1 1 1 1h5v-1a2 2 0 0 1 .01-.2 4.49 4.49 0 0 1 1.534-3.693Q8.844 9.002 8 9c-5 0-6 3-6 4m7 0a1 1 0 0 1 1-1v-1a2 2 0 1 1 4 0v1a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1zm3-3a1 1 0 0 0-1 1v1h2v-1a1 1 0 0 0-1-1" /></svg>{" "}
                 <span style={{ cursor: "pointer" }}>ورود به پنل ادمین</span>
-              </Link>}
+              </Link></>}
               {userData.role === "Owner" && <Link to="/OwnerPanel" aria-label="پنل مالک" className="ProfileMenu-btn">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-fill-lock" viewBox="0 0 16 16">
                   <path d="M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0m-9 8c0 1 1 1 1 1h5v-1a2 2 0 0 1 .01-.2 4.49 4.49 0 0 1 1.534-3.693Q8.844 9.002 8 9c-5 0-6 3-6 4m7 0a1 1 0 0 1 1-1v-1a2 2 0 1 1 4 0v1a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1zm3-3a1 1 0 0 0-1 1v1h2v-1a1 1 0 0 0-1-1" /></svg>{" "}
